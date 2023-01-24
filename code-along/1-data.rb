9e612bf97a98adbdbf54387696e5698767902402
jsg4hw <jc.gamble@kellogg.northwestern.edu>
2023-01-24 20:01:47 +0000

    periodic save

--- code-along/1-data.rb
+++ code-along/1-data.rb
@@ -1,19 +1,42 @@
 # To run this code, be sure your current working directory
-# is the same as where this file is located and then run:
+# is the same as where this file is located:
+# cd code-along
+#and then run:
 # ruby 1-data.rb
 
 # In Ruby, there are different *types* of data:
 
-# Numbers
+# # Numbers
+# puts 5
+# #to output --> puts
+# puts 5+2
+# puts 5-2
+# puts 5*2
+# puts 5/2 #will return an integer because it expects that
+# puts 5%2 #will return the remainder
 
-# Perform simple math with numbers
+# puts 5.0/2.0 #need to insert number as float
+# puts 5.0/2 #one more precise data type will prompt it to return the more precise data type
+
+# # Perform simple math with numbers
+# puts 5 * 2 + 1
+# puts 5 * (2 + 1) #order of operations applies
 
 # Strings
 
+puts "test"
+
 # Combine strings together
 
+puts "Tacos are"+" great"
+puts "Tacos" * 3 #writes "Tacos" 3 times
+puts 3 * "Tacos" #doesn't know how to read this
+
 # Variables
+
+
 
 # Combine strings and variables
 
 # String manipulation
+
