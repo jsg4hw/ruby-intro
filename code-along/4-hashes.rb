4c22785ca5f9cd46c9d314b4a6bfefb25e20b55c
jsg4hw <jc.gamble@kellogg.northwestern.edu>
2023-01-24 22:16:23 +0000

    hashes lab complete

--- code-along/4-hashes.rb
+++ code-along/4-hashes.rb
@@ -3,7 +3,37 @@
 # ruby 4-hashes.rb
 
 # Hashes are lists of *key-value pairs*
+# first portion, left of => is the key and the second portion, right of => is the value
+
+# task1 = {
+#     "description" => "make list",
+#     "completed" => true
+# }
+
+profile = {
+    "name" => "Ben",
+    "location" => "Evanston",
+    "timeline" => [
+        {"status" => "Teaching", "time" => "3:58 PM"},
+        {"status" => "driving home", "time" => "4:30 PM"}
+    ]
+}
+
+# puts profile
 
 # Accessing data from the hash
 
+# puts profile["name"] #returns the value saved with the key specified - returns value of key "name"
+
+# extract the data, assign to variable
+name = profile["name"]
+# puts name 
+
+status = profile["timeline"][0]["status"]
+# puts status 
+
 # More Complex Hashes
+
+profile["name"] = "Benjamin"
+profile["age"] = 37
+puts profile
--- labs/4-hashes.rb
+++ labs/4-hashes.rb
@@ -17,11 +17,26 @@ bitcoin_data = JSON.parse(response)
 # Using the Ruby hash `bitcoin_data`, display a summary of
 # Bitcoin data for the user. Something like the output below.
 
+# #Bitcoin value:
+# {
+#     "time":{"updated":"Jan 24, 2023 22:03:00 UTC","updatedISO":"2023-01-24T22:03:00+00:00","updateduk":"Jan 24, 2023 at 22:03 GMT"},
+#     "disclaimer":"This data was produced from the CoinDesk Bitcoin Price Index (USD). Non-USD currency data converted using hourly conversion rate from openexchangerates.org",
+#     "chartName":"Bitcoin",
+#     "bpi":
+#         {"USD":
+#             {
+#                 "code":"USD",
+#                 "symbol":"&#36;",
+#                 "rate":"22,910.4260",
+#                 "description":"United States Dollar",
+#                 "rate_float":22910.426},
+#         "GBP":{"code":"GBP","symbol":"&pound;","rate":"19,143.7687","description":"British Pound Sterling","rate_float":19143.7687},"EUR":{"code":"EUR","symbol":"&euro;","rate":"22,318.0998","description":"Euro","rate_float":22318.0998}}}
+
 # Sample output:
 # 1 Bitcoin is valued at $41405.1046 USD.
 # Your Bitcoin is worth $62107.6569.
 
-# 1. Get input from a user using gets.chomp.
+# 1. Get input from a user using gets.chomp. ((gets takes input))
 puts "How much bitcoin do you have?"
 bitcoin = gets.chomp
 
@@ -29,4 +44,12 @@ bitcoin = gets.chomp
 bitcoin = bitcoin.to_f
 
 # 3. inspect the bitcoin_data hash
-# puts bitcoin_data
+# puts bitcoin_data.keys --> outputs the keys in bitcoin_data to find where you want to go
+# puts bitcoin_data["bpi"].keys  --> goes deeper
+# puts bitcoin_data["bpi"]["USD"].keys --> even deeper --> looking for the numeric
+
+
+bitcoin_value = bitcoin_data["bpi"]["USD"]["rate_float"]
+user_bc_value = bitcoin * bitcoin_value
+puts "1 Bitcoin is valued at $#{bitcoin_value} USD."
+puts "Your Bitcoin is worth $#{user_bc_value} USD."
