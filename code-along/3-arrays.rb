f1bb9c0604313cb191eca62b297806a124691d6b
jsg4hw <jc.gamble@kellogg.northwestern.edu>
2023-01-24 21:44:29 +0000

    Completed Lab

--- code-along/3-arrays.rb
+++ code-along/3-arrays.rb
@@ -4,9 +4,48 @@
 
 # Arrays are lists of things
 
+favorite_foods = [
+    "tacos",
+    "pizza",
+    "ice cream",
+    "sushi"
+]
+
+# puts favorite_foods
+# puts "#{favorite_foods}"
+# puts favorite_foods.inspect
+
+lost = [ 4, 8, 15, 16, 23, 42 ]
+# puts lost
+
+mixed_array = ["tacos", 3, true]
+# puts mixed_array
+
+shopping_lists = [["diapers", "toys", "formula"],["apples","OJ","oreos"]]
+
 # Accessing the array
 
-# Add to the array
+# puts favorite_foods[0] #returns first entry in array
+# puts favorite_foods[1] #returns second entry in array
+# puts favorite_foods[4].inspect #returns a nil value because there is no fifth entry
+# puts favorite_foods[-1] #returns last entry in array
+
+# Add to the array - next three lines are equivalent
+
+# favorite_foods.push "coffee"
+# favorite_foods.append "coffee"
+# favorite_foods << "coffee"
+
+# insert value at specific position
+favorite_foods.insert(3, "test")
+puts favorite_foods
+
 
 # There are lots of fun things you can do with arrays:
 # https://ruby-doc.org/core-2.7.0/Array.html
+
+# figure out number of values in array
+# favorite_foods.count
+# favorite_foods.length
+# favorite_foods.size
+
--- labs/3-arrays.rb
+++ labs/3-arrays.rb
@@ -7,11 +7,29 @@
 # Create two data structures - one for your list of stuff, and one
 # for your friend, e.g. you want milk, eggs, and bacon, and
 # your friend wants beer, cookies, and apples.
+
+my_list = ["milk", "eggs", "bacon"]
+friends_list = ["beer", "cookies", "apples"]
+
 # Programmatically combine the two arrays into a single list,
+
+# shared_list = my_list.append(friends_list)
+shared_list = my_list + friends_list
+puts shared_list
+
+
+
 # sort the result (alphabetically), and write it to the screen.
+
+shared_list = shared_list.sort
+
 # If the two lists contain the same item, only show it once!
+
+puts shared_list.uniq
 # Lastly, display each item in the list prepended with "buy ".
 
+
+shared_list.each {|shared_list| puts "Buy #{shared_list} "}
 # HINTS
 # Learn to read the documentation!
 # https://ruby-doc.org/core-2.7.0/Array.html
